f662b7dd4217e1124bf514b07483be7d4aebeb21
Piotr C <ppciesiolkiewicz@gmail.com>
2015-03-27 16:20:20 +0100

    MainActivity - layout, don't use android.support.v7.app.ActionBarActivity

--- PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/MainActivity.java
+++ PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/MainActivity.java
@@ -1,10 +1,10 @@
 package com.pciesiol.packagetrackingsystemandroid;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
