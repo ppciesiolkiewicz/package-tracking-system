33842e785484bf75603936e5c9b8b6540c90d4b2
Piotr C <ppciesiolkiewicz@gmail.com>
2015-03-27 13:58:59 +0100

    Activity for displaying package details

---
+++ PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/PackageDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.pciesiol.packagetrackingsystemandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class PackageDetailsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package_details);
+    }
+}
