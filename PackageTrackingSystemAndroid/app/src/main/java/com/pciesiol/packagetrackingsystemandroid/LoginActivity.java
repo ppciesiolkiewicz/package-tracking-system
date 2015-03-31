ab825a9176a58854b9d6cebefdb2b2fdf7a4f366
Piotr C <ppciesiolkiewicz@gmail.com>
2015-03-31 23:07:00 +0200

    login

--- PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/LoginActivity.java
+++ PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/LoginActivity.java
@@ -1,13 +1,110 @@
 package com.pciesiol.packagetrackingsystemandroid;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends ActionBarActivity {
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.pciesiol.packagetrackingsystemandroid.database.DatabaseConnector;
+
+public class LoginActivity extends Activity {
+    private EditText usernameEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+
+        usernameEditText = (EditText) findViewById(R.id.username);
+        passwordEditText = (EditText) findViewById(R.id.password);
+
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == R.id.edittext_action_login ||
+                        actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+                    login();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // Set up the submit button click handler
+        Button actionButton = (Button) findViewById(R.id.action_button);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                login();
+            }
+        });
     }
+
+
+    private void login() {
+        DatabaseConnector db = new DatabaseConnector();
+        String username = usernameEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+
+        if (!isInputValid(username, password))
+            return;
+
+        final ProgressDialog dialog = new ProgressDialog(LoginActivity.this);
+        dialog.setMessage(getString(R.string.progress_login));
+        dialog.show();
+
+
+        db.login(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                dialog.dismiss();
+                if (e != null) {
+                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, DispatchActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+
+    private boolean isInputValid(String username, String password) {
+        boolean validationError = false;
+        StringBuilder validationErrorMessage = new StringBuilder(getString(R.string.error_intro));
+        if (username.length() == 0) {
+            validationError = true;
+            validationErrorMessage.append(getString(R.string.error_blank_username));
+        }
+        if (password.length() == 0) {
+            if (validationError) {
+                validationErrorMessage.append(getString(R.string.error_join));
+            }
+            validationError = true;
+            validationErrorMessage.append(getString(R.string.error_blank_password));
+        }
+        validationErrorMessage.append(getString(R.string.error_end));
+
+        if (validationError) {
+            Toast.makeText(LoginActivity.this, validationErrorMessage.toString(), Toast.LENGTH_LONG)
+                    .show();
+            return !validationError;
+        }
+        return !validationError;
+    }
+
 }
