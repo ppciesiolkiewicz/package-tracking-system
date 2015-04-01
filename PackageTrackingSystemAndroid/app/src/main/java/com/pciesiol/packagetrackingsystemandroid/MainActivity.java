e119127a8e6441731520f16e2d31f97e7b89dadf
Piotr C <ppciesiolkiewicz@gmail.com>
2015-04-01 22:15:17 +0200

    main screen of an app  (scanning codes) working

--- PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/MainActivity.java
+++ PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/MainActivity.java
@@ -1,14 +1,143 @@
 package com.pciesiol.packagetrackingsystemandroid;
 
+import android.IntentIntegrator;
+import android.IntentResult;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.pciesiol.packagetrackingsystemandroid.database.Courier;
+import com.pciesiol.packagetrackingsystemandroid.database.Package;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
+    private Button accountInfoButton;
+    private Button scanButton;
+    private Button logoutButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        accountInfoButton = (Button) findViewById(R.id.button_account_info);
+        accountInfoButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyStatusActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        Courier user =  Courier.getCurrentCourier();
+        for(String s : user.getPackagesIds())
+            System.out.println(s);
+
+
+        scanButton = (Button) findViewById(R.id.button_scan);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                scan();
+            }
+        });
+
+        logoutButton = (Button) findViewById(R.id.button_logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Courier.logOut();
+                Intent intent = new Intent(MainActivity.this, DispatchActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void scan() {
+        //instantiate ZXing integration class
+        IntentIntegrator scanIntegrator = new IntentIntegrator(MainActivity.this);
+        //start scanning
+        scanIntegrator.initiateScan(IntentIntegrator.QR_CODE_TYPES);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //ZXing is the only activity resulting here.
+        //retrieve result of scanning - instantiate ZXing object
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        String scanContent;
+        if (scanningResult != null && (scanContent = scanningResult.getContents()) != null) {
+            //valid scan data
+            processCode(scanContent);
+        } else {
+            //invalid scan data or scan canceled
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    getResources().getString(R.string.msg_no_scan_data), Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    private void processCode(String packageCode) {
+        final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+        dialog.setMessage(getString(R.string.progress_checking_the_code));
+        dialog.show();
+
+
+        Package.getQuery().getInBackground(packageCode, new GetCallback<Package>() {
+            public void done(Package pkg, ParseException e) {
+                dialog.dismiss();
+                if (e == null) {
+                    Toast toast;
+                    if(doIHaveThatPackage()) {
+                        toast = Toast.makeText(getApplicationContext(),
+                                getResources().getString(R.string.you_dropped_the_package), Toast.LENGTH_SHORT);
+                    }
+                    else {
+                        toast = Toast.makeText(getApplicationContext(),
+                                getResources().getString(R.string.you_get_the_package), Toast.LENGTH_SHORT);
+                    }
+                    updateMyPackages();
+                    toast.show();
+                } else {
+                    showDialog(getResources().getString(R.string.invalid_package_code));
+                }
+            }
+        });
+    }
+
+    private boolean doIHaveThatPackage() {
+         //TODO
+        return true;
+    }
+
+    private void updateMyPackages() {
+        //TODO
+        List<String> list =  ParseUser.getCurrentUser().getList("packages");
+        for( String l : list )
+            System.out.println(l);
+    }
+
+
+    private void showDialog(String msg) {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setMessage(msg);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 }
