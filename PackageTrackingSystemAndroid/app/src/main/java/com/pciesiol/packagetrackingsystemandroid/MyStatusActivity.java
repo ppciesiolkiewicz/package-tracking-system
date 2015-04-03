eb475f0e2b5e4054b5a0c700fb81116d84bc8250
Piotr C <ppciesiolkiewicz@gmail.com>
2015-04-03 18:09:44 +0200

    current courier status activity, package details - problem with transferring packages as serializable

--- PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/MyStatusActivity.java
+++ PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/MyStatusActivity.java
@@ -1,13 +1,54 @@
 package com.pciesiol.packagetrackingsystemandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.pciesiol.packagetrackingsystemandroid.database.Courier;
+import com.pciesiol.packagetrackingsystemandroid.database.Package;
+
+import java.util.List;
 
 public class MyStatusActivity extends Activity {
+    private List<Package> packages;
+    private ListView packagesListView;
+    private ArrayAdapter<Package> packagesAdapter;
+    private MyStatusActivity self; //this
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_status);
+        self = this;
+        packagesListView = (ListView) findViewById(R.id.packagesListView);
+
+
+        Package.getCourierPackagesQuery(Courier.getCurrentCourier().getCourierId()).findInBackground(new FindCallback<Package>() {
+            @Override
+            public void done(List<Package> packages_, ParseException e) {
+                packages = packages_;
+                packagesAdapter = new ArrayAdapter<>(self, android.R.layout.simple_list_item_1, packages);
+                packagesListView.setAdapter(packagesAdapter);
+            }
+        });
+
+        packagesListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Package pkg = packagesAdapter.getItem(position);
+                Intent intent = new Intent(MyStatusActivity.this,PackageDetailsActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("package", pkg);
+                //intent.putExtra("package", pkg);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
     }
 }
--- PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/PackageDetailsActivity.java
+++ PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/PackageDetailsActivity.java
@@ -1,13 +1,39 @@
 package com.pciesiol.packagetrackingsystemandroid;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+import com.pciesiol.packagetrackingsystemandroid.database.Package;
 
-public class PackageDetailsActivity extends ActionBarActivity {
+public class PackageDetailsActivity extends Activity {
+
+    private TextView packageIdTextView;
+    private TextView packageDescriptionTextView;
+    private TextView packageWeightTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_package_details);
+
+        Intent intent = getIntent();
+        Bundle bundle=intent.getExtras();
+        //Package pkg = (Package) intent.getSerializableExtra("package");
+        Package pkg = (Package) bundle.getSerializable("package");
+        initFields();
+        setPackageInfo(pkg);
+    }
+
+    private void initFields() {
+        this.packageIdTextView = (TextView) findViewById(R.id.packageIdTextView);
+        this.packageDescriptionTextView = (TextView) findViewById(R.id.packageDescriptionTextView);
+        this.packageWeightTextView = (TextView) findViewById(R.id.packageWeightTextView);
+    }
+
+    public void setPackageInfo(Package packageInfo) {
+        this.packageIdTextView.setText(packageInfo.getPackageId() == null ? packageInfo.getPackageId() : "nima");
+        this.packageDescriptionTextView.setText(packageInfo.getDescription() == null ? packageInfo.getDescription() : "nima");
+        this.packageWeightTextView.setText(packageInfo.getWeight() == null ? Integer.toString(packageInfo.getWeight()) : "nima");
     }
 }
