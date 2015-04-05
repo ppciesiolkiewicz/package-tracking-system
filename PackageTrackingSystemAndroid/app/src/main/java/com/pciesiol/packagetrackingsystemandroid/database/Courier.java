1be87c9cd0e67edbaae348432ea1c3ba7718ca89
Piotr C <ppciesiolkiewicz@gmail.com>
2015-04-05 23:46:46 +0200

    small fix in updating courier location

--- PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/database/Courier.java
+++ PackageTrackingSystemAndroid/app/src/main/java/com/pciesiol/packagetrackingsystemandroid/database/Courier.java
@@ -1,6 +1,9 @@
 package com.pciesiol.packagetrackingsystemandroid.database;
 
+import android.location.Location;
+
 import com.parse.ParseClassName;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseUser;
 
 @ParseClassName("_User")
@@ -10,6 +13,14 @@ public class Courier extends ParseUser {
 
     public static Courier getCurrentCourier() {
         return (Courier) ParseUser.getCurrentUser();
+    }
+
+    public static void updateCurrentCourierPosition(Location location) {
+        ParseGeoPoint geoPoint = new ParseGeoPoint(location.getLatitude(), location.getLongitude());
+        Courier c = Courier.getCurrentCourier();
+        c.put("position", geoPoint);
+        c.saveInBackground();
+        System.out.println(location.getLatitude() + " " + location.getLongitude());
     }
 
     public String getCourierId() { return this.getObjectId(); }
